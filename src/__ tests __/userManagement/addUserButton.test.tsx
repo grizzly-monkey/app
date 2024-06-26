b20b61eba2a94e76e5dcbea5620271d347122a9a
Nahid <nahid@glabbr.tech>
2024-06-26 09:32:36 +0530

    GD-75: test writen wwith mockup store

--- src/__ tests __/userManagement/addUserButton.test.tsx
+++ src/__ tests __/userManagement/addUserButton.test.tsx
@@ -1,42 +1,45 @@
-// import AddUserButton from "@/pages/userManagement/addUserButton";
-// import { fireEvent, render,screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// jest.mock("react-redux", () => ({
-//   useDispatch: jest.fn(),
-//   useSelector: (callback: any) => callback(),
-// }));
-// jest.mock("@/redux/user/actions", () => ({
-//   createUser: jest.fn(),
-// }));
-// jest.mock("@/redux/requesting/requestingSelector", () =>
-//   jest.fn().mockReturnValue(false)
-// );
-// describe("Add User Button", () => {
-//   it("should render modal on button click", () => {
-//       const component = render(<AddUserButton />);
-//       const button = component.getByRole("button", { name: /Add User/i });
-//       expect(button).toBeInTheDocument();
-//       fireEvent.click(button);
-//       expect(component.getByTestId("add-user-modal")).toBeInTheDocument();
-//   });
+import AddUserButton from "@/pages/userManagement/addUserButton";
+import { fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { setupDefaultStore } from "../utils/setupTests";
+import { renderWithProvider } from "../utils/testUtils";
+
+describe("Add User Button", () => {
+  let store: any;
+
+  beforeEach(() => {
+    store = setupDefaultStore({
+      users: {
+        users: {},
+        selectedUser: null,
+      },
+    });
+  });
+  //   it("should render modal on button click", () => {
+  //      renderWithProvider(<AddUserButton />, { store });
+  //       const button = screen.getByRole("button", { name: /Add User/i });
+  //       expect(button).toBeInTheDocument();
+  //       fireEvent.click(button);
+  //       expect(screen.getByTestId("add-user-modal")).toBeInTheDocument();
+  //   });
 //   it("should close modal on cancel", () => {
-//       const component = render(<AddUserButton />);
-//       const button = component.getByRole("button", { name: /Add User/i });
-//       fireEvent.click(button);
-//       const cancelButton = component.getByRole("button", { name: /Cancel/i });
-//       fireEvent.click(cancelButton);
-//       expect(component.queryByTestId("add-user-modal")).not.toBeInTheDocument();
-//   });
-//   it("should display error message on clicking ok without filling form", async () => {
-//     render(<AddUserButton />);
+//     renderWithProvider(<AddUserButton />, { store });
 //     const button = screen.getByRole("button", { name: /Add User/i });
 //     fireEvent.click(button);
-
-//     const okButton = screen.getByRole("button", { name: "Add" });
-//     fireEvent.click(okButton);
-
-//     expect(
-//       await screen.findByText("Please input first name")
-//     ).toBeInTheDocument();
+//     const cancelButton = screen.getByRole("button", { name: /Cancel/i });
+//     fireEvent.click(cancelButton);
+//     expect(screen.queryByTestId("add-user-modal")).not.toBeInTheDocument();
 //   });
-// });
+    it("should display error message on clicking ok without filling form", async () => {
+      renderWithProvider(<AddUserButton />,{store});
+      const button = screen.getByRole("button", { name: /Add User/i });
+      fireEvent.click(button);
+
+      const okButton = screen.getByRole("button", { name: "Add" });
+      fireEvent.click(okButton);
+
+      expect(
+        await screen.findByText("Please input first name")
+      ).toBeInTheDocument();
+    });
+});
